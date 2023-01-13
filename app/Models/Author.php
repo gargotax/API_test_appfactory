0835eba2b5f481766007992d221a033090ce4b99
gargotax <109099566+gargotax@users.noreply.github.com>
2023-01-13 11:55:32 +0100

    aggiunta migration

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,16 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 /**
  * @property integer id
  * @property string name
  * @property string lastname
- * @property string birthday
+ *  * @property string birthday
+
  */
 class Author extends Model
 {
     use HasFactory;
 
 
-    protected $fillable = ['name', 'surname', 'birthday'];
+    protected $fillable = ['name', 'lastname', 'birthday'];
 }
